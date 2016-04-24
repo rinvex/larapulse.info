3ebdcf787e727ecbc6082b6e6afd2c47ea89e596
Abdelrahman Omran <me@omranic.com>
2016-04-24 11:44:09 +0200

    Add "A week of Laravel (17-24 April 2016)" report

--- content/_includes/sidebar.blade.php
+++ content/_includes/sidebar.blade.php
@@ -7,7 +7,7 @@
 <hr />
 
 <strong>Laravel:</strong><br />
-- <strong>5.2:</strong> <a href="https://github.com/laravel/framework/releases/tag/v5.2.29">v5.2.29</a><br />
+- <strong>5.2:</strong> <a href="https://github.com/laravel/framework/releases/tag/v5.2.30">v5.2.30</a><br />
 - <strong>5.1:</strong> <a href="https://github.com/laravel/framework/releases/tag/v5.1.34">v5.1.34</a><br />
 - <strong>5.0:</strong> <a href="https://github.com/laravel/framework/releases/tag/v5.0.35">v5.0.35</a><br />
 
